63ac270a27137f4ee8ddf6b7e0b0de9c3ead538a
christopherpoland <chris.poland94@gmail.com>
2019-02-27 16:35:09 -0800

    Template of page complete, css further organized
    Beginning design of media query for smaller devices (mobile)

--- scripts/tester.js
+++ scripts/tester.js
@@ -1,18 +1,21 @@
-var highlight
 document.addEventListener("click", function(event) {
   console.log(event.target.id);
-      if (event.target.id === "expand" && event.target.parentElement.parentElement.parentElement.parentElement.querySelector(".expanded-content").classList.contains('hidden')) { //.preview-box is hidden
-        console.log("yo")
-        event.target.parentElement.parentElement.parentElement.parentElement.querySelector(".preview-box").classList.toggle('hidden'); //.preview-box
-        event.target.parentElement.parentElement.parentElement.parentElement.querySelector(".expanded-content").classList.toggle('hidden'); //.expanded-content
-      }
-      else if (event.target.id === "collapse" && event.target.parentElement.parentElement.parentElement.querySelector(".preview-box").classList.contains('hidden')) { //.preview-box is hidden
-        console.log("yo1")
-        event.target.parentElement.parentElement.parentElement.querySelector(".preview-box").classList.toggle('hidden'); //.preview-box
-        event.target.parentElement.parentElement.parentElement.querySelector(".expanded-content").classList.toggle('hidden'); //.expanded-content
-      }
-
-
-
-
+  if (event.target.id === "expandIcon" && event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(".expanded-content").classList.contains('hidden')) { //.preview-box is hidden
+    event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(".preview-box").classList.toggle('hidden'); //.preview-box
+    event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(".expanded-content").classList.toggle('hidden'); //.expanded-content
+    }
+  else if (event.target.id === "collapseIcon" && event.target.parentElement.parentElement.parentElement.parentElement.querySelector(".preview-box").classList.contains('hidden')) { //.preview-box is hidden
+    event.target.parentElement.parentElement.parentElement.parentElement.querySelector(".preview-box").classList.toggle('hidden'); //.preview-box
+    event.target.parentElement.parentElement.parentElement.parentElement.querySelector(".expanded-content").classList.toggle('hidden'); //.expanded-content
+  }
 });
+document.getElementById("smallNav").addEventListener("click", function(event) {
+  modalDisplay(event);
+});
+document.getElementById("navbarWrapper").addEventListener("click", function(event) {
+  modalDisplay(event);
+});
+function modalDisplay(event) {
+  if (event.target.id === "smallNav" || event.target.id !== "navbar" && event.target.id !== "funny")
+    document.getElementById("navbarWrapper").classList.toggle('hidden-mobile');
+}
