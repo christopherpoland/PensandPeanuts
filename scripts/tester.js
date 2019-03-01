725546e323579a97af9c6bc1b1a1153a05688764
christopherpoland <chris.poland94@gmail.com>
2019-03-01 15:20:04 -0800

    Finished modal for mobile
    Organized css classes further to trim js, added js and css to provide functionality to modal

--- scripts/tester.js
+++ scripts/tester.js
@@ -12,15 +12,64 @@ document.addEventListener("click", function(event) {
     event.target.parentElement.parentElement.parentElement.parentElement.querySelector(".preview-box").scrollIntoView(); //moves viewport back to collapsed element
   }
 });
-document.getElementById("smallNav").addEventListener("click", function(event) {
+document.getElementById("smallNav").addEventListener("click", function(event) { //Opens or exits modal
   modalDisplay(event);
+  document.body.style.overflow = "hidden";
 });
-document.getElementById("navbarWrapper").addEventListener("click", function(event) {
-  modalDisplay(event);
+document.addEventListener("click", function(event) { //Exits Modal
+  if (event.target.id === "navbarWrapper") {
+    modalDisplay(event);
+    expandModal();
+  }
+});
+document.getElementById("nameInput").addEventListener("click", function(event) {
+  expandModal();
+
+});
+document.getElementById("emailInput").addEventListener("click", function(event) {
+  expandModal();
+});
+document.getElementById("messageInput").addEventListener("click", function(event) {
+  expandModal();
 });
 function modalDisplay(event) {
-  if (event.target.id === "smallNav" || event.target.id !== "navbar" && event.target.id !== "funny")
-    document.getElementById("navbarWrapper").classList.toggle('hidden-mobile');
+    var modal = document.getElementById("navbarWrapper");
+    var titleNames = ["Pens","Pages","People","Places","Peanuts"];
+    if (window.getComputedStyle(modal).display === "none") { //display modal
+      modal.style.display = "flex";
+    }
+    else { //hides modal
+      modal.style.display = "none";
+      document.body.style.overflow = "auto";
+      navbar.classList.remove('navbar-extend');
+      navbar.classList.add('navbar-initial');
+      document.getElementById("contactMobile").classList.remove("contact-mobile-extended");
+      navbar.querySelectorAll("a").forEach((element,index) => {
+        element.style.textAlign = "left";
+        element.innerHTML = titleNames[index];
+      });
+    }
+}
+function expandModal() {
+  var navbar = document.getElementById("navbar");
+  var emojis = ["🖊️","📖","👪","⛺","🌰"];
+  //console.log(navbar.querySelectorAll("a"));
+  //Check if width is 40% or 90%
+  if (Math.round((window.getComputedStyle(navbar).width).replace("px","")) === Math.round(window.innerWidth * 0.4)) { //expands
+    navbar.classList.add('navbar-extend');
+    navbar.classList.remove('navbar-initial');
+    document.getElementById("contactMobile").classList.add("contact-mobile-extended");
+
+    navbar.querySelectorAll("a").forEach((element,index) => {
+      element.style.textAlign = "center";
+      element.innerHTML = emojis[index];
+    });
+  }
+  /*else { //extended
+    navbar.classList.remove('navbar-extend');
+    navbar.classList.add('navbar-initial');
+    document.getElementById("contactMobile").classList.remove("contact-mobile-extended");
+  } */
 }
 function discernPhotoType() {
   var previewPhotos = document.querySelectorAll(".preview-photo");
