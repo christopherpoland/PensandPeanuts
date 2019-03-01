66beb814f4c7c08ceaad0d68f87610b086e1565e
christopherpoland <chris.poland94@gmail.com>
2019-02-28 17:26:33 -0800

    Added mobile CSS + JS
    Added scrollTo and CSS/JS to deal with aspect ratios of photos

--- scripts/tester.js
+++ scripts/tester.js
@@ -1,12 +1,15 @@
+discernPhotoType();
 document.addEventListener("click", function(event) {
   console.log(event.target.id);
   if (event.target.id === "expandIcon" && event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(".expanded-content").classList.contains('hidden')) { //.preview-box is hidden
     event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(".preview-box").classList.toggle('hidden'); //.preview-box
     event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(".expanded-content").classList.toggle('hidden'); //.expanded-content
+    event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(".expanded-content").scrollIntoView(); //moves viewport back to collapsed element
     }
   else if (event.target.id === "collapseIcon" && event.target.parentElement.parentElement.parentElement.parentElement.querySelector(".preview-box").classList.contains('hidden')) { //.preview-box is hidden
     event.target.parentElement.parentElement.parentElement.parentElement.querySelector(".preview-box").classList.toggle('hidden'); //.preview-box
     event.target.parentElement.parentElement.parentElement.parentElement.querySelector(".expanded-content").classList.toggle('hidden'); //.expanded-content
+    event.target.parentElement.parentElement.parentElement.parentElement.querySelector(".preview-box").scrollIntoView(); //moves viewport back to collapsed element
   }
 });
 document.getElementById("smallNav").addEventListener("click", function(event) {
@@ -18,4 +21,17 @@ document.getElementById("navbarWrapper").addEventListener("click", function(even
 function modalDisplay(event) {
   if (event.target.id === "smallNav" || event.target.id !== "navbar" && event.target.id !== "funny")
     document.getElementById("navbarWrapper").classList.toggle('hidden-mobile');
+}
+function discernPhotoType() {
+  var previewPhotos = document.querySelectorAll(".preview-photo");
+  previewPhotos.forEach(element => {
+    var height = window.getComputedStyle(element).height;
+    var width = window.getComputedStyle(element).width;
+    if (height > width) {
+      element.classList.add('portrait');
+    }
+    else {
+      element.classList.add('landscape')
+    }
+  });
 }
